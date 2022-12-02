81be5e4f949da3489755d48aa7bc35bfb5dd9f39
BRITTANY BLALOCK <brittany.blalock2@gmail.com>
2022-12-02 10:30:31 -0800

    adding code to js server

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,6 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDB = async (content) => {
+  console.log("PUT to the database");
+
+  const contactDb = await openDB("jate", 1);
+
+  //New transaction and specify the database and data privileges.
+  const tx = contactDb.transaction("jate", "readandwrite");
+
+  //Open up the desired object store
+  const store = tx.objectStore("jate");
+
+  //Use the .add() method on the store and pass in the content
+  const request = store.put({ id: 1, value: content });
+
+  //Confirmation of the request
+  const result = await request;
+  console.log("The data is saved to the database", result);
+};
+
 export const putDb = async (content) => console.error("putDb not implemented");
 
 // TODO: Add logic for a method that gets all the content from the database
