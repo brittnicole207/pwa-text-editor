e38f67ebb94a0fd0de7d87c5a4590b1c3555e245
BRITTANY BLALOCK <brittany.blalock2@gmail.com>
2022-12-02 10:15:44 -0800

    commiting after updating server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,12 @@
-const express = require('express');
-const mongoose = require ('mongoose');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-
-
-
-
-require('./routes/htmlRoutes')(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
